ac6d6ca94905ca4cf8e6c21c9ea0a8ddbae8bcaf
julian-yamori <julian.lampyridae@gmail.com>
2025-07-19 16:24:00 +0000

    LegacyCommandsApp のコードを分離

---
+++ src/legacy_commands/legacy_commands_app.rs
@@ -0,0 +1,51 @@
+use std::sync::Arc;
+
+use eframe::egui::{self, RichText, mutex::Mutex};
+
+use crate::legacy_commands::{console::Console, navigation::LegacyCommandsNavigation};
+
+#[derive(Default)]
+pub struct LegacyCommandsApp {
+    console: Arc<Mutex<Console>>,
+    navigation: LegacyCommandsNavigation,
+}
+
+impl LegacyCommandsApp {
+    pub fn show(&mut self, ui: &mut egui::Ui) {
+        ui.vertical(|ui| {
+            self.navigation.show_tab(ui);
+            let page = &mut *self.navigation.current_page;
+
+            ui.separator();
+
+            ui.vertical_centered(|ui| {
+                ui.add_space(4.0);
+
+                // コマンドの簡易説明タイトル
+                ui.label(RichText::new(page.page_discription()).heading().strong());
+
+                ui.add_space(10.0);
+
+                // パラメータの入力欄
+                page.show_form(ui);
+
+                ui.add_space(10.0);
+
+                // 実行ボタン
+                let button = ui.button(RichText::new("実行").heading());
+                if button.clicked() {
+                    page.run_command(self.console.clone());
+                }
+
+                ui.add_space(4.0);
+            });
+
+            ui.separator();
+
+            // Console area
+            ui.label("Console:");
+            ui.add_space(5.0);
+            self.console.lock().show(ui);
+        });
+    }
+}
